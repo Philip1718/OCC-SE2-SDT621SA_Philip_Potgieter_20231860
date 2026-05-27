2d3d61b19314d56bbe0bea80a0d716266b1d8e82
Philip1718 <127308771+Philip1718@users.noreply.github.com>
2026-05-27 10:14:28 +0200

    Add files via upload

---
+++ SDT621_SA_Question_1/SDT621_SA_Question_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDT621_SA_Question_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Welcome to the Structure of a C# Program Demo!\n");
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {name}");
+
+            
+           
+                Console.Write("Program Structure Demostrated:\n");
+                Console.Write("1. using System: imports functionality\n");
+                Console.Write("2. namespace : organize code\n");
+                Console.Write("3. class Program : container for code\n");
+                Console.Write("4. Main() : entry point of program\n");
+                Console.Write("5. Commets : explain logic and documention\n");
+
+            Console.WriteLine("Program  executed successfully!");
+                
+            
+
+            
+        }
+    }
+}
