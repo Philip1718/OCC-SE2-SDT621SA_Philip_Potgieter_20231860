fa49c7852c211c8d976b96ce52dbb9c904c36e84
Philip1718 <127308771+Philip1718@users.noreply.github.com>
2026-05-27 11:14:28 +0200

    Add files via upload
    This is Sections C.

---
+++ SDT621_Section_C_1/SDT621_Section_C_1/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace SDT621_Section_C_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.txtMake = new System.Windows.Forms.TextBox();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.tblMobilePhones = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(371, 35);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(44, 16);
+            this.lblOutput.TabIndex = 0;
+            this.lblOutput.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(177, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mobile Code";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(177, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Make";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(177, 208);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Quantity";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(180, 272);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(285, 272);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(374, 272);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(75, 23);
+            this.btnFind.TabIndex = 6;
+            this.btnFind.Text = "Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(268, 114);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(100, 22);
+            this.txtCode.TabIndex = 7;
+            // 
+            // txtMake
+            // 
+            this.txtMake.Location = new System.Drawing.Point(268, 157);
+            this.txtMake.Name = "txtMake";
+            this.txtMake.Size = new System.Drawing.Size(100, 22);
+            this.txtMake.TabIndex = 8;
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(268, 201);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(100, 22);
+            this.txtQuantity.TabIndex = 9;
+            // 
+            // tblMobilePhones
+            // 
+            this.tblMobilePhones.FormattingEnabled = true;
+            this.tblMobilePhones.ItemHeight = 16;
+            this.tblMobilePhones.Location = new System.Drawing.Point(395, 114);
+            this.tblMobilePhones.Name = "tblMobilePhones";
+            this.tblMobilePhones.Size = new System.Drawing.Size(330, 132);
+            this.tblMobilePhones.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tblMobilePhones);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.txtMake);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblOutput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.TextBox txtMake;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.ListBox tblMobilePhones;
+    }
+}
+
---
+++ SDT621_Section_C_1/SDT621_Section_C_1/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace SDT621_Section_C_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        // This is to add a Mobile to the list
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+            if (txtCode.Text == "")
+            {
+                MessageBox.Show("Please enter the mobile code");
+                return;
+            }
+
+
+            if (txtMake.Text == "")
+            {
+                MessageBox.Show("Please enter in the make.");
+                return;
+            }
+
+            if (txtQuantity.Text == "")
+            {
+                MessageBox.Show("Please enter in the quantity");
+                return;
+            }
+
+
+            double code = double.Parse(txtCode.Text);
+            double qty = double.Parse(txtQuantity.Text);
+            string make = txtMake.Text;
+            
+
+            if (qty < 0)
+            {
+                MessageBox.Show("Quantity cannot be negative.");
+                return;
+            }
+
+
+            string mobileData = "Mobile Code: " + code + "  Make:" + make + " - Quantity:" + qty.ToString("N2");
+            tblMobilePhones.Items.Add(mobileData);
+
+            // CLEAN UP
+            lblOutput.Text = "Mobile added Successfully!";
+            txtCode.Clear();
+            txtMake.Clear();
+            txtQuantity.Clear();
+
+        }
+
+        // This is to Delete a mobile
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+            if (tblMobilePhones.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select a Mobile to remove");
+                return;
+            }
+
+            tblMobilePhones.Items.Remove(tblMobilePhones.SelectedItem);
+            lblOutput.Text = "Mobile has been removed";
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            // VALIDATION: Check if search field is empty
+            if (txtCode.Text == "")
+            {
+                MessageBox.Show("Please enter in the Mobile Code");
+                return;
+            }
+
+            string searchName = txtCode.Text;
+            bool found = false;
+
+            // LOOP through the ListBox to find the name
+            for (int i = 0; i < tblMobilePhones.Items.Count; i++)
+            {
+                if (tblMobilePhones.Items[i].ToString().Contains(searchName))
+                {
+                    found = true;
+                    break;
+                }
+            }
+
+            // SHOW RESULT
+            if (found == true)
+            {
+                lblOutput.Text = "Mobile has been found in the system!";
+            }
+            else
+            {
+                lblOutput.Text = "No Mobile has been found.";
+            }
+        }
+    }
+}
+
